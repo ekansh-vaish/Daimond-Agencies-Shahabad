b88eb0fc1db555a35b2fbf1c07682357bf8a1f96
Ekansh Vaish <ekansh982@gmail.com>
2025-10-01 01:04:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -79,6 +79,12 @@ res.status(status).json({ error: message });
 });
 
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 
 
